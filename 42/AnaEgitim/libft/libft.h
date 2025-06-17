f11a5d63398ccca66b7e3c3a14720e0477cf469e
Windows <talhayilmz@hotmail.com>
2025-06-17 20:27:08 +0300

    atoi

--- 42/AnaEgitim/libft/libft.h
+++ 42/AnaEgitim/libft/libft.h
@@ -6,6 +6,7 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int ft_atoi(char *str);
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
@@ -13,6 +14,7 @@ int	ft_isdigit(int c);
 int	ft_isprint(int c);
 int	ft_toupper(int c);
 int ft_tolower(int c);
+
 
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
